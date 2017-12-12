344e575ed8df3543d06da6baa3a983b88f165b90
piotrhor11 <piotrhor11@gmail.com>
2017-12-12 13:18:37 +0100

    Excerise3: MANIFEST.MF created for jar build; Exercise3.javaFx: validation added to amount field, console field added, auto scroll functionality added to a console field, data initialization moved to MainJavaFxController

--- src/main/java/javaFx/MainJavaFx.java
+++ src/main/java/javaFx/MainJavaFx.java
@@ -1,51 +1,31 @@
 package javaFx;
 
-import commons.CryptoCurrencyDTO;
-import http.Exercise3;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 
 public class MainJavaFx extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-//        Parent root = FXMLLoader.load(getClass().getResource("/MainJavaFx.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/MainJavaFx.fxml"));
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainJavaFx.fxml"));
-        Pane pane = loader.load();
-
-        //Getting instance of the controller (declared in fxml) to initialize data in choiceBox
-        MainJavaFxController controller = loader.getController();
-        HashSet<CryptoCurrencyDTO> ccDTOHashSet = Exercise3.getCryptoCurrencyDTOHashSet();
-
-        List<String> list = new ArrayList<>();
-        for (CryptoCurrencyDTO ccDTO: ccDTOHashSet) {
-            list.add(ccDTO.getName());
-        }
-        ObservableList obList = FXCollections.observableList(list);
-        ChoiceBox<String> choiceBox = controller.getChoiceBox();
-        choiceBox.setItems(obList);
-        choiceBox.getSelectionModel().select(0);            //Default selection
-        controller.setChoiceBox(choiceBox);
+        //Alternative approach, which allows us also to extract controller
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainJavaFx.fxml"));
+//        Pane pane = loader.load();
+//        MainJavaFxController controller = loader.getController();         //extracting controller
 
         primaryStage.setTitle("Crypto currency exchange rates");
-        primaryStage.setScene(new Scene(pane));
+        primaryStage.setScene(new Scene(root));
+//        primaryStage.setScene(new Scene(pane));       //Scene setting for the alternative approach
         primaryStage.show();
     }
 
-//    public static void main() {
+//    public static void main() {           //In case we would like to start our javaFx application from here
 //        launch();
 //    }
 
--- src/main/java/javaFx/MainJavaFxController.java
+++ src/main/java/javaFx/MainJavaFxController.java
@@ -2,16 +2,21 @@ package javaFx;
 
 import commons.CryptoCurrencyDTO;
 import http.Exercise3;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 import java.math.BigDecimal;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MainJavaFxController implements Initializable {
@@ -24,6 +29,9 @@ public class MainJavaFxController implements Initializable {
 
     @FXML
     private TextField amount;
+
+    @FXML
+    private TextArea console;
 
     @FXML
     private void convert(ActionEvent event){
@@ -40,20 +48,38 @@ public class MainJavaFxController implements Initializable {
             }
         }
         result.setText((number.multiply(rate)).toString());
+        console.appendText("\nLook above for your result :)");
     }
-
-    public ChoiceBox<String> getChoiceBox() {
-        return choiceBox;
-    }
-
-    public void setChoiceBox(ChoiceBox<String> choiceBox) {
-        this.choiceBox = choiceBox;
-    }
-
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        //Getting data from Exercise3 class and use it to populate initial data for choiceBox
+        HashSet<CryptoCurrencyDTO> ccDTOHashSet = Exercise3.getCryptoCurrencyDTOHashSet();
+        List<String> list = new ArrayList<>();
+        for (CryptoCurrencyDTO ccDTO: ccDTOHashSet) {
+            list.add(ccDTO.getName());
+        }
+        ObservableList obList = FXCollections.observableList(list);
+        choiceBox.setItems(obList);
+        choiceBox.getSelectionModel().select(0);            //Default selection
+
+        //Adds auto scroll functionality to TextArea console
+        console.textProperty().addListener(((observable, oldValue, newValue) -> {
+            console.setScrollTop(Double.MAX_VALUE);
+        }));
+
+        //Input validation - accepts only numeric characters
+        amount.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(!newValue.matches("\\d{0,7}([\\.]\\d{0,4})?")){
+                amount.setText(oldValue);
+                console.appendText("\n\"Amount\" accepts only a numeric values");
+            }
+        });
+
+
+
+        console.setText("Choose Crypto Currency and enter an amount. The convert button will give you its current equivalent in USD");
     }
 }
